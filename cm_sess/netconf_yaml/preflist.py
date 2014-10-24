e5068cb1a353d1c2531fdf4b74ce0af49f02e479
tamihiro <dead6oy@gmail.com>
2014-10-24 22:24:33 +0900

    use yaml, unnecessiate xml parsing

---
+++ cm_sess/netconf_yaml/preflist.py
@@ -0,0 +1,7 @@
+"""
+Pythonifier for prefix-list Table/View
+"""
+from jnpr.junos.factory import loadyaml
+from os.path import splitext
+_YAML_ = splitext(__file__)[0] + '.yml'
+globals().update(loadyaml(_YAML_))
